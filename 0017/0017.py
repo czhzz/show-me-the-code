f44527b6c06fad25c0f7ee66ceb0d77e41e7fb8f
caozh <913272571@qq.com>
2018-08-10 16:10:10 +0800

    modify

--- 0017/0017.py
+++ 0017/0017.py
@@ -11,23 +11,23 @@ import json
 from lxml import etree
 
 def read_excel(file_name):
-    exl = xlrd.open_workbook(file_name, encoding_override='utf-8')
-    exl_sheet = exl.sheet_by_name('student')
-    data = {}
-    for i in range(exl_sheet.nrows):
-        data[exl_sheet.row_values(i)[0]] = exl_sheet.row_values(i)[1:]
-    return json.dumps(data, ensure_ascii=False)
+  exl = xlrd.open_workbook(file_name, encoding_override='utf-8')
+  exl_sheet = exl.sheet_by_name('student')
+  data = {}
+  for i in range(exl_sheet.nrows):
+      data[exl_sheet.row_values(i)[0]] = exl_sheet.row_values(i)[1:]
+  return json.dumps(data, ensure_ascii=False)
 
 def save_to_xml(data, new_file_name):
-    root = etree.Element('root')
-    students = etree.SubElement(root, 'students')
-    # students.append(etree.Comment(u"""学生信息表 "id" : [名字, 数学, 语文, 英文]"""))
-    students.text = data
+  root = etree.Element('root')
+  students = etree.SubElement(root, 'students')
+  # students.append(etree.Comment(u"""学生信息表 "id" : [名字, 数学, 语文, 英文]"""))
+  students.text = data
 
-    student_xml = etree.ElementTree(root)
-    student_xml.write(new_file_name, pretty_print=True, xml_declaration=True, encoding='utf-8')
+  student_xml = etree.ElementTree(root)
+  student_xml.write(new_file_name, pretty_print=True, xml_declaration=True, encoding='utf-8')
 
 
 if __name__ == '__main__':
-    content = read_excel('student.xls')
-    save_to_xml(content, 'student.xml')
+  content = read_excel('student.xls')
+  save_to_xml(content, 'student.xml')
--- 0018/0018.py
+++ 0018/0018.py
@@ -11,22 +11,22 @@ import json
 from lxml import etree
 
 def read_excel(file_name):
-    exl = xlrd.open_workbook(file_name, encoding_override='utf-8')
-    exl_sheet = exl.sheet_by_name('city')
-    data = {}
-    for i in range(exl_sheet.nrows):
-      data[exl_sheet.row_values(i)[0]] = exl_sheet.row_values(i)[1]
-    return json.dumps(data, ensure_ascii=False)
+  exl = xlrd.open_workbook(file_name, encoding_override='utf-8')
+  exl_sheet = exl.sheet_by_name('city')
+  data = {}
+  for i in range(exl_sheet.nrows):
+    data[exl_sheet.row_values(i)[0]] = exl_sheet.row_values(i)[1]
+  return json.dumps(data, ensure_ascii=False)
 
 def save_to_xml(data, new_file_name):
-    root = etree.Element('root')
-    students = etree.SubElement(root, 'city')
-    students.text = data
+  root = etree.Element('root')
+  students = etree.SubElement(root, 'city')
+  students.text = data
 
-    student_xml = etree.ElementTree(root)
-    student_xml.write(new_file_name, pretty_print=True, xml_declaration=True, encoding='utf-8')
+  student_xml = etree.ElementTree(root)
+  student_xml.write(new_file_name, pretty_print=True, xml_declaration=True, encoding='utf-8')
 
 
 if __name__ == '__main__':
-    content = read_excel('city.xls')
-    save_to_xml(content, 'city.xml')
+  content = read_excel('city.xls')
+  save_to_xml(content, 'city.xml')
